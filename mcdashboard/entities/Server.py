e69972885a2efd39c08b4049344a67703cc597a4
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-08-04 21:25:51 +0200

    added port field to Server

--- mcdashboard/entities/Server.py
+++ mcdashboard/entities/Server.py
@@ -1,10 +1,12 @@
 from mongoengine import (
     Document,
-    StringField
+    StringField,
+    IntegerField
 )
 
 
 class Server(Document):
     name = StringField(max_length=120, unique=True)
+    port = IntegerField()
     container_id = StringField(max_length=1000, unique=True)
     # creator = {ReferenceField(User)}
